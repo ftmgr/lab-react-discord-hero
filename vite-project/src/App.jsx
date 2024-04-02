6cfce3082f980f2d4af6032e1b47f607f1267bc1
Fatma <fatmagurses1@gmail.com>
2024-04-02 07:45:38 +0200

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+//import { useState } from 'react'
+import discordLogo from './assets/discord-logo-white.png'
+import menuIcon from './assets/menu-icon.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <p> you are rocking it!</p>
+        <img src={menuIcon} className="" alt="Menu icon" />
+
+        <img src={discordLogo} className="" alt="Discord logo" />
+      </div>
+
+    </>
+  );
+}
+
+export default App
+
+
